3ea5ce60770dbce9bb9112a4dec329148d4ad417
jubayerjuhan <davidjuhan23@gmail.com>
2024-10-28 15:39:00 +0600

    fix: deleted admin get route from logout

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -1,11 +1,5 @@
 import { cookies } from "next/headers";
 
-export async function AdminPanel() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("token");
-
-  // ...
-}
 export async function GET(request: Request) {
   const cookieStore = await cookies();
   const address = cookieStore.get("token");
